50766c459d0d4d81ead27b26fac1a448be1ab215
Sewery <seweryn.tasior@op.pl>
2024-10-20 20:22:18 +0200

    Minor changes

--- oolab/src/main/java/agh/ics/oop/OptionsParser.java
+++ oolab/src/main/java/agh/ics/oop/OptionsParser.java
@@ -3,28 +3,20 @@ package agh.ics.oop;
 import agh.ics.oop.model.MoveDirection;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class OptionsParser {
-    public static List<MoveDirection> convertToMoveDirections(String[] args) {
+    public static MoveDirection[] convertToMoveDirections(String[] args) {
         var moveDirections = new ArrayList<MoveDirection>();
         for (String arg : args) {
-            if (arg.equals("f")
-                    || arg.equals("b")
-                    || arg.equals("r")
-                    || arg.equals("l")
-            ) {
-                moveDirections.add(switch (arg) {
-                    case "f" -> MoveDirection.FORWARD;
-                    case "b" -> MoveDirection.BACKWARD;
-                    case "r" -> MoveDirection.RIGHT;
-                    case "l" -> MoveDirection.LEFT;
-                    default -> throw new IllegalArgumentException("Invalid MoveDirection");
-                });
+            switch (arg) {
+                case "f" -> moveDirections.add(MoveDirection.FORWARD);
+                case "b" -> moveDirections.add(MoveDirection.BACKWARD);
+                case "r" -> moveDirections.add(MoveDirection.RIGHT);
+                case "l" -> moveDirections.add(MoveDirection.LEFT);
             }
 
         }
-        return moveDirections;
+        return moveDirections.toArray(MoveDirection[]::new);
     }
 }
 
--- oolab/src/main/java/agh/ics/oop/World.java
+++ oolab/src/main/java/agh/ics/oop/World.java
@@ -5,20 +5,22 @@ import agh.ics.oop.model.MoveDirection;
 public class World {
     public static void run(String[] args) {
         var moveDirections= OptionsParser.convertToMoveDirections(args);
+        final String ANIMAL_GO = "Zwierzak idzie ";
+        final String ANIMAL_TURN = "Zwierzak skreca ";
         for (var move : moveDirections) {
             switch(move){
-                case FORWARD-> System.out.println("zwierzak idzie do przodu");
-                case BACKWARD-> System.out.println("zwierzak idzie do tylu");
-                case RIGHT-> System.out.println("zwierzak skreca w prawo");
-                case LEFT-> System.out.println("zwierzak skreca w lewo");
+                case FORWARD-> System.out.println(ANIMAL_GO + "do przodu");
+                case BACKWARD-> System.out.println(ANIMAL_GO + "do tylu");
+                case RIGHT-> System.out.println(ANIMAL_TURN + "w prawo");
+                case LEFT-> System.out.println(ANIMAL_TURN + "w lewo");
             };
         }
-        System.out.println(String.join(", ",args));
+        //System.out.println(String.join(", ",args));
     }
     public static void main(String[] args) {
-        System.out.println("system wystartowal");
+        System.out.println("System wystartowal");
         run(args);
-        System.out.println("system zakonczyl dzialanie");
+        System.out.println("System zakonczyl dzialanie");
 
 
     }
