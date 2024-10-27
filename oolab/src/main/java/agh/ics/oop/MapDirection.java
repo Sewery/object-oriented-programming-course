2097aa61c34acca0a61c81fea67873694b8c3bfd
Sewery <seweryn.tasior@op.pl>
2024-10-27 14:44:39 +0100

    Small changes to class MapDirection

--- oolab/src/main/java/agh/ics/oop/MapDirection.java
+++ oolab/src/main/java/agh/ics/oop/MapDirection.java
@@ -15,24 +15,24 @@ public enum MapDirection {
         return  directionPol;
     }
 
-    public static MapDirection previous(MapDirection direction) {
-        return switch(direction){
+    public MapDirection previous() {
+        return switch(this){
             case NORTH -> WEST;
             case WEST -> SOUTH;
             case SOUTH -> EAST;
             case EAST -> NORTH;
         };
     }
-    public static MapDirection next(MapDirection direction) {
-        return switch(direction){
+    public MapDirection next() {
+        return switch(this){
             case NORTH -> EAST;
             case EAST -> SOUTH;
             case SOUTH -> WEST;
             case WEST -> NORTH;
         };
     }
-    public static Vector2d toUnitVector(MapDirection direction){
-        return switch (direction){
+    public Vector2d toUnitVector(){
+        return switch (this){
             case NORTH -> new Vector2d(0,1);
             case EAST -> new Vector2d(-1,0);
             case SOUTH -> new Vector2d(0,-1);
