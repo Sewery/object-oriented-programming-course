b1231124f6b460e186382e018c3edc6a51afff4b
Sewery <seweryn.tasior@op.pl>
2024-10-27 13:47:23 +0100

    Adding class Vector2d

---
+++ oolab/src/main/java/agh/ics/oop/Vector2d.java
@@ -0,0 +1,53 @@
+package agh.ics.oop;
+
+import static java.lang.Math.max;
+import static java.lang.Math.min;
+
+public class Vector2d {
+    private final int x;
+    private final int y;
+
+    public Vector2d(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+    @Override
+    public String toString() {
+        return "(" + x + ", " + y + ")";
+    }
+    boolean precedes(Vector2d other){
+        return x <= other.x && y <= other.y;
+    }
+    boolean follows(Vector2d other){
+        return x >= other.x && y >= other.y;
+    }
+    Vector2d add(Vector2d other){
+        return new Vector2d(x+other.getX(), y+other.getY());
+    }
+    Vector2d subtract(Vector2d other){
+        return new Vector2d(x-other.getX(), y-other.getY());
+    }
+    Vector2d upperRight(Vector2d other){
+        return new Vector2d(max(x,other.getX()), max(y,other.getY()));
+    }
+    Vector2d lowerLeft(Vector2d other){
+        return new Vector2d(min(x,other.getX()), min(y,other.getY()));
+    }
+    Vector2d opposite(){
+        return new Vector2d(-x, -y);
+    }
+    @Override
+    public boolean equals(Object other){
+        if(other instanceof Vector2d v){
+            return x == v.getX() && y == v.getY();
+        }
+        return false;
+    }
+}
--- oolab/src/main/java/agh/ics/oop/World.java
+++ oolab/src/main/java/agh/ics/oop/World.java
@@ -18,10 +18,14 @@ public class World {
         //System.out.println(String.join(", ",args));
     }
     public static void main(String[] args) {
-        System.out.println("System wystartowal");
-        run(args);
-        System.out.println("System zakonczyl dzialanie");
+//        System.out.println("System wystartowal");
+//        run(args);
+//        System.out.println("System zakonczyl dzialanie");
 
-
+        Vector2d position1 = new Vector2d(1,2);
+        System.out.println(position1);
+        Vector2d position2 = new Vector2d(-2,1);
+        System.out.println(position2);
+        System.out.println(position1.add(position2));
     }
 }
