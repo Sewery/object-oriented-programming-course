3da9276035c47fde74ac696c19db741baa3d0f1c
Sewery <seweryn.tasior@op.pl>
2024-10-27 16:23:18 +0100

    Refactoring World and OptionsParser class

--- oolab/src/main/java/agh/ics/oop/World.java
+++ oolab/src/main/java/agh/ics/oop/World.java
@@ -3,29 +3,27 @@ package agh.ics.oop;
 import agh.ics.oop.model.MoveDirection;
 
 public class World {
-    public static void run(String[] args) {
-        var moveDirections= OptionsParser.convertToMoveDirections(args);
-        final String ANIMAL_GO = "Zwierzak idzie ";
-        final String ANIMAL_TURN = "Zwierzak skreca ";
+    private static void run(String[] userInput) {
+        var moveDirections= OptionsParser.convertToMoveDirections(userInput);
         for (var move : moveDirections) {
             switch(move){
-                case FORWARD-> System.out.println(ANIMAL_GO + "do przodu");
-                case BACKWARD-> System.out.println(ANIMAL_GO + "do tylu");
-                case RIGHT-> System.out.println(ANIMAL_TURN + "w prawo");
-                case LEFT-> System.out.println(ANIMAL_TURN + "w lewo");
+                case FORWARD-> System.out.println("Zwierzak idzie do przodu");
+                case BACKWARD-> System.out.println("Zwierzak idzie do tylu");
+                case RIGHT-> System.out.println("Zwierzak skreca w prawo");
+                case LEFT-> System.out.println("Zwierzak skreca w lewo");
             };
         }
         //System.out.println(String.join(", ",args));
     }
     public static void main(String[] args) {
-//        System.out.println("System wystartowal");
-//        run(args);
-//        System.out.println("System zakonczyl dzialanie");
+        System.out.println("System wystartowal");
+        run(args);
+        System.out.println("System zakonczyl dzialanie");
 
-        Vector2d position1 = new Vector2d(1,2);
-        System.out.println(position1);
-        Vector2d position2 = new Vector2d(-2,1);
-        System.out.println(position2);
-        System.out.println(position1.add(position2));
+//        Vector2d position1 = new Vector2d(1,2);
+//        System.out.println(position1);
+//        Vector2d position2 = new Vector2d(-2,1);
+//        System.out.println(position2);
+//        System.out.println(position1.add(position2));
     }
 }
