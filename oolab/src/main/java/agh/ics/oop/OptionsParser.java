4ca14de59a930802964ff464f3a87f47bd8c8e59
Seweryn Tasior <72692913+Sewery@users.noreply.github.com>
2024-10-28 14:36:24 +0100

    Revert "Lab2 do oceny"

--- oolab/src/main/java/agh/ics/oop/OptionsParser.java
+++ oolab/src/main/java/agh/ics/oop/OptionsParser.java
@@ -5,10 +5,10 @@ import agh.ics.oop.model.MoveDirection;
 import java.util.ArrayList;
 
 public class OptionsParser {
-    public static MoveDirection[] convertToMoveDirections(String[] userInput) {
+    public static MoveDirection[] convertToMoveDirections(String[] args) {
         var moveDirections = new ArrayList<MoveDirection>();
-        for (String element : userInput) {
-            switch (element) {
+        for (String arg : args) {
+            switch (arg) {
                 case "f" -> moveDirections.add(MoveDirection.FORWARD);
                 case "b" -> moveDirections.add(MoveDirection.BACKWARD);
                 case "r" -> moveDirections.add(MoveDirection.RIGHT);
