9367a54a95c1e6a3d39589d91fa061c95721394d
Sewery <seweryn.tasior@op.pl>
2024-11-11 15:29:07 +0100

    Adding requested changes and new tests

--- oolab/src/test/java/agh/ics/oop/SimulationTest.java
+++ oolab/src/test/java/agh/ics/oop/SimulationTest.java
@@ -1,14 +1,12 @@
 package agh.ics.oop;
 
-import agh.ics.oop.model.Animal;
-import agh.ics.oop.model.MoveDirection;
-import agh.ics.oop.model.Vector2d;
+import agh.ics.oop.model.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class SimulationTest {
     private Simulation simulation;
@@ -16,15 +14,16 @@ public class SimulationTest {
     @BeforeEach
     void setUp() {
         var positions = List.of(new Vector2d(2, 2), new Vector2d(3, 3));
-        var movements = List.of(MoveDirection.FORWARD, MoveDirection.RIGHT, MoveDirection.FORWARD);
-        simulation = new Simulation(positions, movements);
+        var movements = List.of(MoveDirection.FORWARD, MoveDirection.RIGHT, MoveDirection.FORWARD,MoveDirection.FORWARD);
+        WorldMap worldMap = new RectangularMap(4,4);
+        simulation = new Simulation(positions,movements, worldMap);
     }
 
     @Test
     void initialPositions() {
         var animals = simulation.getAnimals();
-        assertEquals(new Vector2d(2, 2), animals.get(0).getPosition());
-        assertEquals(new Vector2d(3, 3), animals.get(1).getPosition());
+        assertEquals(new Vector2d(2, 2), animals.get(0).getCurrPosition());
+        assertEquals(new Vector2d(3, 3), animals.get(1).getCurrPosition());
     }
 
     @Test
@@ -33,20 +32,12 @@ public class SimulationTest {
         List<Animal> animals = simulation.getAnimals();
 
         // Check final positions and orientations
-        assertEquals(new Vector2d(2, 3), animals.get(0).getPosition());
-        assertEquals("Wschod", animals.get(0).getOrientation().toString());
+        assertEquals(new Vector2d(2, 4), animals.get(0).getCurrPosition());
+        assertEquals("N", animals.get(0).getOrientation().toString());
 
-        assertEquals(new Vector2d(3, 4), animals.get(1).getPosition());
-        assertEquals("Polnoc", animals.get(1).getOrientation().toString());
+
+        assertEquals(new Vector2d(4, 3), animals.get(1).getCurrPosition());
+        assertEquals("E", animals.get(1).getOrientation().toString());
     }
 
-    @Test
-    void outOfBoundsMovement() {
-        List<Vector2d> invalidPositions = List.of(new Vector2d(-1, -1));
-        List<MoveDirection> movements = List.of(MoveDirection.FORWARD);
-
-        assertThrows(IllegalArgumentException.class, () -> {
-            new Simulation(invalidPositions, movements);
-        });
-    }
 }
